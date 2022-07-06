bf6d0320b038a28b3a7c36bbd0e458c3b988b681
felipe.machado <felipe.machado@warrenbrasil.com.br>
2022-07-05 22:22:15 -0300

    feat: added RestCountries API client.

---
+++ src/RestCountries.Client/Models/Country.cs
@@ -0,0 +1,10 @@
+﻿namespace RestCountries.Client.Models
+{
+    public class Country
+    {
+        public CountryName Name { get; set; }
+        public string CCA2 { get; set; }
+        public string CCA3 { get; set; }
+        public int Population { get; set; }
+    }
+}
---
+++ src/RestCountries.Client/ServiceCollectionExtensions.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Polly;
+using Polly.Extensions.Http;
+using System.Net.Http.Headers;
+
+namespace RestCountries.Client
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddRestCountriesClient(this IServiceCollection services,
+                                                          string baseUri,
+                                                          int maxRetryAttempts,
+                                                          int maxConsecutiveFailures,
+                                                          int maxCircuitBreakerWaitingTime,
+                                                          TimeSpan timeout = default)
+        {
+            if (services == null)
+            {
+                throw new ArgumentNullException(nameof(services));
+            }
+
+            services.AddHttpClient(nameof(IRestCountriesClient), configureClient: httpClient => ConfigureClient(httpClient, baseUri, timeout))
+                .AddPolicyHandler(GetDefaultRetryPolicy(maxRetryAttempts))
+                .AddPolicyHandler(GetDefaultCircuitBreakerPolicy(maxConsecutiveFailures, maxCircuitBreakerWaitingTime));
+
+            services.AddScoped<IRestCountriesClient, RestCountriesClient>();
+
+            return services;
+        }
+
+        private static void ConfigureClient(HttpClient httpClient, string baseUri, TimeSpan? timeout = null)
+        {
+            httpClient.BaseAddress = new Uri($"{baseUri}/");
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            if (timeout.HasValue)
+            {
+                httpClient.Timeout = timeout.Value;
+            }
+        }
+
+        private static IAsyncPolicy<HttpResponseMessage> GetDefaultRetryPolicy(int maxRequestAttempt)
+            => HttpPolicyExtensions
+                .HandleTransientHttpError()
+                .WaitAndRetryAsync(maxRequestAttempt, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)));
+
+        private static IAsyncPolicy<HttpResponseMessage> GetDefaultCircuitBreakerPolicy(int maxConsecutiveFailures, int maxCircuitBreakerWaitingTime)
+            => HttpPolicyExtensions
+                .HandleTransientHttpError()
+                .CircuitBreakerAsync(
+                    handledEventsAllowedBeforeBreaking: maxConsecutiveFailures,
+                    durationOfBreak: TimeSpan.FromSeconds(maxCircuitBreakerWaitingTime),
+                    onBreak: (delegateResult, timeSpan, context) => { },
+                    onReset: (context) => { });
+    }
+}
