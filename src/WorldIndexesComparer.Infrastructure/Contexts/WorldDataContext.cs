de19649e26fd33cb3f0c5b031b724091e0a60c0a
Felipe Coelho Machado <felipe.machado@warrenbrasil.com.br>
2022-07-06 21:40:22 -0300

    architecture/infrastructure-data (#5)
    * architecture: added data layer
    
    * feat: added Country mapping.
    
    * architecture: added docker-compose file.

---
+++ src/WorldIndexesComparer.Infrastructure/Contexts/WorldDataContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WorldIndexesComparer.Infrastructure.Data.Contexts
+{
+    public class WorldDataContext : DbContext
+    {
+        public WorldDataContext(DbContextOptions<WorldDataContext> options)
+           : base(options)
+        {
+            ChangeTracker.LazyLoadingEnabled = false;
+            ChangeTracker.AutoDetectChangesEnabled = false;
+            ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfigurationsFromAssembly(typeof(WorldDataContext).Assembly);
+        }
+    }
+}
---
+++ src/WorldIndexesComparer.Infrastructure/Mappings/CountryMap.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using WorldIndexesComparer.Domain.Countries;
+
+namespace WorldIndexesComparer.Infrastructure.Data.Mappings
+{
+    public class CountryMap : IEntityTypeConfiguration<Country>
+    {
+        public void Configure(EntityTypeBuilder<Country> builder)
+        {
+            // Primary Key
+            builder.HasKey(x => x.Id);
+
+            // Properties
+            builder.Property(x => x.Name)
+                .IsRequired()
+                .HasMaxLength(256);
+
+            builder.Property(x => x.CountryCode)
+                .IsRequired()
+                .HasMaxLength(3);
+
+            builder.Property(x => x.Population)
+                .IsRequired();
+        }
+    }
+}
