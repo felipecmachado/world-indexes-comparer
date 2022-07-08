db022d2ff92333e064c8c3e247c977016167f69a
Felipe Coelho Machado <felipe.machado@warrenbrasil.com.br>
2022-07-08 09:20:51 -0300

    feat/countries-sync (#7)
    * feat: added countries sync service.
    
    * fix: fixed wrong reference.

---
+++ tests/WorldIndexesComparer.Tests/Application/Services/CountriesProcessingServiceTests.cs
@@ -0,0 +1,60 @@
+﻿using EntityFrameworkCore.UnitOfWork.Interfaces;
+using FluentAssertions;
+using Microsoft.Extensions.Logging;
+using Moq;
+using RestCountries.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using WorldIndexesComparer.Application.Services;
+using Xunit;
+
+namespace WorldIndexesComparer.Tests.Application.Services
+{
+    public class CountriesProcessingServiceTests
+    {
+        private readonly Mock<ILogger<CountriesProcessingService>> _mockLogger;
+        private readonly Mock<IUnitOfWork> _mockUnitOfWork;
+        private readonly Mock<IRestCountriesClient> _mockRestCountriesClient;
+
+        private readonly CountriesProcessingService _countriesProcessingService;
+
+        public CountriesProcessingServiceTests()
+        {
+            _mockLogger = new();
+            _mockUnitOfWork = new();
+            _mockRestCountriesClient = new();
+            _countriesProcessingService = new(_mockLogger.Object, _mockUnitOfWork.Object, _mockRestCountriesClient.Object);
+        }
+
+        [Fact]
+        public void Constructor_Initialize_ThrowArgumentNullException()
+        {
+            // Arrange
+
+            // Act
+            Action actionLoggerNull = () => _ = new CountriesProcessingService(null, _mockUnitOfWork.Object, _mockRestCountriesClient.Object);
+            Action actionUnitOfWorkNull = () => _ = new CountriesProcessingService(_mockLogger.Object, null, _mockRestCountriesClient.Object);
+            Action actionRestCountriesClientNull = () => _ = new CountriesProcessingService(_mockLogger.Object, _mockUnitOfWork.Object, null);
+
+            // Assert
+            actionLoggerNull.Should().Throw<ArgumentNullException>();
+            actionUnitOfWorkNull.Should().Throw<ArgumentNullException>();
+            actionRestCountriesClientNull.Should().Throw<ArgumentNullException>();
+        }
+
+        [Fact]
+        public async Task SyncAllCountriesAsync_ValidData_RunSuccessfully()
+        {
+            // Arrange
+
+            // Act
+            // await _countriesProcessingService.SyncAllCountriesAsync(new CancellationToken());
+
+            // Assert
+        }
+    }
+}
