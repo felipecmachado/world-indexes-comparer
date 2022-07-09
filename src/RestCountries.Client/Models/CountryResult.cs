5aa62c910a2f12528d6b44e10fceafea392e568d
felipe.machado <felipe.machado@warrenbrasil.com.br>
2022-07-09 14:24:23 -0300

    fix: fixed some properties that were not being deserialized.

--- src/RestCountries.Client/Models/CountryResult.cs
+++ src/RestCountries.Client/Models/CountryResult.cs
@@ -1,10 +1,17 @@
-﻿namespace RestCountries.Client.Models
+﻿using System.Text.Json.Serialization;
+
+namespace RestCountries.Client.Models
 {
     public class CountryResult
     {
         public CountryNameResult Name { get; set; }
+
+        [JsonPropertyName("cca2")]
         public string CCA2 { get; set; }
+
+        [JsonPropertyName("cca3")]
         public string CCA3 { get; set; }
+
         public int Population { get; set; }
     }
 }
