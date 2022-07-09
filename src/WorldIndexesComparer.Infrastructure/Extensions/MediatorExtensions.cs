6bcd55f66e8f17d5f36506feb025758f5bd81507
felipe.machado <felipe.machado@warrenbrasil.com.br>
2022-07-09 14:17:40 -0300

    architecture: added mediator extensions.

---
+++ src/WorldIndexesComparer.Infrastructure/Extensions/MediatorExtensions.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using WorldIndexesComparer.Domain;
+
+namespace WorldIndexesComparer.Infrastructure.Extensions;
+
+public static class MediatorExtensions
+{
+    public static async Task DispatchDomainEventsAsync(this IMediator mediator, Entity entity, CancellationToken cancellationToken)
+    {
+
+        foreach (var @event in entity.DomainEvents)
+        {
+            if (@event is INotification notification)
+            {
+                await mediator.Publish(notification, cancellationToken).ConfigureAwait(continueOnCapturedContext: false);
+            }
+        }        
+
+        entity.ClearDomainEvents();
+    }
+}
